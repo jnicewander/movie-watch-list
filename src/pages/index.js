5dd3824777a154900a8e9f6e353ebe547790f5c4
Justin Nicewander <jnicewander@live.com>
2021-06-27 21:24:48 -0400

    Add search input handlers.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react"
 import SearchBar from "../components/search-bar"
+import Movies from "../components/movies"
+import { useGetMoviesApi } from "../hooks/useGetMoviesApi"
 
 const IndexPage = () => {
   const [userInput, setUserInput] = useState('')
+  const [{ data, status }, doFetch] = useGetMoviesApi();  
+  const { results: movies = [] } = data;
+
+  const handleChange = event => {
+    setUserInput(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    doFetch(userInput)
+  }
 
   return (
     <main>
       <title>Home || Movie Watch List</title>
       <SearchBar
         value={userInput}
-        setValue={setUserInput}
-      />      
+        onSubmit={e => handleSubmit(e)}
+        onChange={e => handleChange(e)}
+      />
     </main>
   )
 }
